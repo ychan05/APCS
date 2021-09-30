78a2122e4937fcc97265b1c5fd288f923de2fa8a
ychan2005 <ychan30@stuy.edu>
2021-09-30 16:35:49 -0400

    hellofxn

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,11 @@
+public class Greet{
+	public static void greet(String name){
+		System.out.println("Hello " +  name+ "!");
+	}
+
+	public static void main(String [] args){
+		greet("Joshua");
+		greet("Mr. Mykolyk");
+		greet("David");
+	}
+}
