a7183b20f03168f61d6f1347adc517ee4623ca1d
ychan2005 <ychan30@stuy.edu>
2021-10-01 16:43:30 -0400

    finished code

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,20 +1,6 @@
-public class BigSib{
-
-	/*
-	DISCOVERIES
-	A method consists of (in order) a return type, method name, and parameters.
-	
-	UNRESOLVED QUESTIONS
-	none
-	*/
- 
+public class BigSib{ 
 	public static void greet(String name){
 		System.out.println("Hello " +  name + "!");
 	}
 
-	public static void main(String [] args){
-		greet("Joshua");
-		greet("Mr. Mykolyk");
-		greet("David");
-	}
 }
--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,6 +1,7 @@
-import BigSib.greet;
 public class Greet{
 	public static void main(String [] args){
-		
+		BigSib.greet("David");
+		BigSib.greet("Ben");
+		BigSib.greet("Mr.Mykolyk");
 	}
 }
