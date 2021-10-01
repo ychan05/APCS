2c2efb69a382169be231b4f1c1315e1f0649bc13
Yat Long Chan <77934108+ychan2005@users.noreply.github.com>
2021-10-01 16:51:00 -0400

    added heading

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,12 +1,20 @@
+/*
+Yat Long Chan
+APCS
+HW08: Refactor Freshie Zero
+2021-09-30
+*/
+
+/*
+DISCOVERIES
+A method consists of (in order) a return type, method name, and parameters.
+UNRESOLVED QUESTIONS
+none
+*/
+
 public class Greet{
 
-	/*
-	DISCOVERIES
-	A method consists of (in order) a return type, method name, and parameters.
-	
-	UNRESOLVED QUESTIONS
-	none
-	*/
+
  
 	public static void greet(String name){
 		System.out.println("Hello " +  name + "!");
