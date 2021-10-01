8c687b2d84f3b0c4994176b0090539695133288f
ychan2005 <ychan30@stuy.edu>
2021-09-30 20:18:39 -0400

    added notes

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,6 +1,15 @@
 public class Greet{
+
+	/*
+	DISCOVERIES
+	A method consists of (in order) a return type, method name, and parameters.
+	
+	UNRESOLVED QUESTIONS
+	none
+	*/
+ 
 	public static void greet(String name){
-		System.out.println("Hello " +  name+ "!");
+		System.out.println("Hello " +  name + "!");
 	}
 
 	public static void main(String [] args){
