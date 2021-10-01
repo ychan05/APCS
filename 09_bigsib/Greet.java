844eaeb5e865293ee1d1bb990ee64f1b6f3eb88c
Yat Long Chan <77934108+ychan2005@users.noreply.github.com>
2021-10-01 16:45:58 -0400

    added notes

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,4 +1,10 @@
 public class Greet{
+	/* DISCOVERIES
+	   You can use a method in another .java file as long as the two files are in the same directory.
+	   
+	   UNRESOLVED QUESTIONS
+	   none
+	*/
 	public static void main(String [] args){
 		BigSib.greet("David");
 		BigSib.greet("Ben");
