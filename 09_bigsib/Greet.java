cee852d3d7526a7aeedd7643ef85030d09bc9222
Yat Long Chan <77934108+ychan2005@users.noreply.github.com>
2021-10-01 16:52:16 -0400

    added heading

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,10 +1,17 @@
+/*
+Yat Long Chan
+APCS
+HW09: What Are BigSibs Good For?
+2021-10-01
+*/
+
+/* DISCOVERIES
+You can use a method in another .java file as long as the two files are in the same directory.
+UNRESOLVED QUESTIONS
+none
+*/
+
 public class Greet{
-	/* DISCOVERIES
-	   You can use a method in another .java file as long as the two files are in the same directory.
-	   
-	   UNRESOLVED QUESTIONS
-	   none
-	*/
 	public static void main(String [] args){
 		BigSib.greet("David");
 		BigSib.greet("Ben");
