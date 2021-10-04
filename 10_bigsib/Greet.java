46e40b9bbaf3f54cd3a583adebba75170db54a3a
ychan2005 <ychan30@stuy.edu>
2021-10-04 16:19:55 -0400

    hw 10

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,20 @@
+/*
+BDY: Yat Long Chan, Ben Belotser, David Chen, Eugene, Bob, TwoFace
+APCS
+HW10: Refactor Big Sib One
+2021-10-04
+
+
+DISCOVERIES
+You have to state return type before method name.
+UNRESOLVED QUESTIONS
+none
+*/
+
+public class Greet{
+	public static void main(String [] args){
+		System.out.println(BigSib.greet("David"));
+		System.out.println(BigSib.greet("Ben"));
+		System.out.println(BigSib.greet("Mr.Mykolyk"));
+	}
+}
