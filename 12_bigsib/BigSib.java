1813818ce53b8157efc1a30e42a3fc87f31522a9
Yat Long Chan <77934108+ychan2005@users.noreply.github.com>
2021-10-05 16:21:36 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,13 +1,12 @@
 /*
 BDY: Yat Long Chan, Ben Belotser, David Chen, Eugene, Bob, TwoFace
 APCS
-HW10: Refactor Big Sib One
-2021-10-04
+HW10: On Elder Individuality and the Elimination of Radio Fuzz
+2021-10-05
 
-
-DISCOVERIES
-You have to state return type before method name.
-UNRESOLVED QUESTIONS
+DISCO
+Instance vars can only be used in non static methods.
+QCC
 none
 */
 
