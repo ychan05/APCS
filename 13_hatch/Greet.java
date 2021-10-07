6ddc3506771526fb055e6da90c20a30415cb4885
Yat Long Chan <77934108+ychan2005@users.noreply.github.com>
2021-10-06 22:34:19 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -16,8 +16,8 @@ public class Greet {
 	public static void main( String[] args ) {
     		String greeting;
 
-    		BigSib richard = new BigSib();
-
+    		BigSib richard = new BigSib("Word up");
+		
     		greeting = richard.greet("freshman");
     		System.out.println(greeting);
   }
