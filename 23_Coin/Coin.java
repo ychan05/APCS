1803970e11c0708090c23d152dcb6ee2aa0e9a35
Yat Long Chan <77934108+ychan2005@users.noreply.github.com>
2021-10-22 19:26:34 -0400

    Update Coin.java

--- 23_Coin/Coin.java
+++ 23_Coin/Coin.java
@@ -1,175 +1,48 @@
 /***
- *  class Coin
- *  by Clyde "Thluffy" Sinclair
- *  SKELETON
- ***/
+    driver for class Coin
+    ~~~ SUGGESTED WORKFLOW: ~~~
+    1. Compile this file and run. Note anything notable.
+    2. Move the "TOP" line down, so that it is below the first statement.
+    (emacs: with cursor at beginning of TOP line, C-k C-k, DOWN, DOWN, C-y)
+    (your editor: ???)
+    3. Compile and run again.
+    4. Resolve errors one at a time until it works.
+    5. Repeat 2-4 until TOP meets BOTTOM.
+***/
 
-public class Coin {
+public class Driver {
 
-  //attributes aka instance vars
-  private double value;
-  private String upFace;
-  private String name;
-  private int flipCtr;
-  private int headsCtr;
-  private int tailsCtr;
-  private double bias;
+  public static void main( String[] args ) {
 
-  /***
-   *  Coin() -- default constuctor
-   *  precond:
-   *  postcond:
-   ***/
-  public Coin() {
-    bias = 0.5;
-    upFace = "heads";
-  }
+    //build Objects from blueprint specified by class Coin
 
-  /***
-      Coin(String) -- overloaded constructor
-      precond: input is one of
-      "penny",
-      "nickel",
-      "dime",
-      "quarter",
-      "half dollar",
-      "dollar"
-      postcond:
-  ***/
-  public Coin(String s) {
-    name = s;
-    upFace = "heads";
-  }
 
-  /***
-      Coin(String,String) --
-      precond:
-      postcond:
-  ***/
-  public Coin(String s, String nowFace) {
-    name = s;
-    upFace = nowFace;
-  }
-
-  // Accessors...
-  // ----------------------------
-  public String getUpFace() {
-    return upFace;
-  }
-
-  public int getFlipCtr() {
-    return flipCtr;
-  }
-
-  public double getValue() {
-    return value;
-  }
-
-  public int getHeadsCtr() {
-    return headsCtr;
-  }
-
-  public int getTailsCtr() {
-    return tailsCtr;
-  }
-  // ----------------------------
-
-  /***
-   * assignValue() -- set a Coin's monetary value based on its name
-   * precond:  input String is a valid coin name ("penny", "nickel", etc.)
-   * postcond: instvar value gets appropriate value
-   * Returns value assigned.
-   ***/
-  private double assignValue(String s) {
-    if (s == "penny") {
-      value = 0.01;
-      return value;
-    } else if (s == "nickel") {
-      value = 0.05;
-      return value;
-    } else if (s == "dime") {
-      value = 0.10;
-      return value;
-    } else if (s == "quarter") {
-      value = 0.25;
-      return value;
-    } else if (s == "half dollar") {
-      value = 0.50;
-      return value;
-    } else if (s == "dollar") {
-      value = 1.0;
-      return value;
-    } else {
-      System.out.println("Enter valid coin name in lowercase");
-      return 999999;
-    }
-  }
-
-  /***
-      reset() -- initialize a Coin
-      precond:  s is "heads" or "tails", 0.0 <= d <= 1.0
-      postcond: Coin's attribs reset to starting vals
-  ***/
-  public void reset(String s, double d) {
-    if ((s == "heads" || s == "tails") && (d >= 0.0 && d <= 1.0)) {
-      upFace = "";
-      bias = 0.5;
-    } else {
-      System.out.println("Preconditions not satisfied.");
-    }
-  }
-
-  /***
-   * String flip() -- simulates a Coin flip
-   * precond:  bias is a double on interval [0.0,1.0]
-   * (1.0 indicates always heads, 0.0 always tails)
-   * postcond: upFace updated to reflect result of flip.
-   * flipCtr incremented by 1.
-   * Either headsCtr or tailsCtr incremented by 1, as approp.
-   * Returns "heads" or "tails"
-   ***/
-  public String flip() {
-    flipCtr += 1;
-    bias = Math.random();
-    double flipProb = Math.random();
-    if (flipProb < bias) {
-      upFace = "heads";
-      headsCtr += 1;
-      return "heads";
-    } else {
-      upFace = "tails";
-      tailsCtr += 1;
-      return "tails";
-    }
-  }
-
-  /***
-   * boolean equals(Coin) -- checks to see if 2 coins have same face up
-   * precond: other is not null
-   * postcond: Returns true if both coins showing heads
-   * or both showing tails. False otherwise.
-   ***/
-  public boolean equals(Coin other) {
-    if (other == null) {
-      System.out.println("Param object does not exist");
-      return false;
-    } else {
-      if (other.upFace == "tails" && upFace == "tails" || other.upFace == "heads" && upFace == "heads") {
-        return true;
-      } else {
-        return false;
+    //test default constructor
+    
+      Coin mine = new Coin();
+      //test 1st overloaded constructor
+      Coin yours = new Coin( "quarter" );
+      //test 2nd overloaded constructor
+      Coin wayne = new Coin( "dollar", "heads" );
+      //test toString() methods of each Coin
+      System.out.println("mine: " + mine);
+      System.out.println("yours: " + yours);
+      System.out.println("wayne: " + wayne);
+      //test flip() method
+      System.out.println("\nAfter flipping...");
+      yours.flip();
+      wayne.flip();
+      System.out.println("yours: " + yours);
+      System.out.println("wayne: " + wayne);
+      //test equals() method
+      if ( yours.equals(wayne) ) {
+        System.out.println( "Matchee matchee!" );
       }
-    }
-  }
+      else {
+        System.out.println( "No match. Firestarter you can not be." );
+      }
+      
 
-  /***
-   * String toString() -- overrides toString() provided by Java
-   * precond: n/a
-   * postcond: Return String comprised of name and current face
-   ***/
-  public String toString() {
-    String retStr = name + " -- " + upFace;
-    return retStr;
-  }
+  }//end main()
 
-} //end class
+}//end class
