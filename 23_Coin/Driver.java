71bf1a8d6ce604512b95b286b76cb574513fca70
Yat Long Chan <77934108+ychan2005@users.noreply.github.com>
2021-10-22 22:29:09 -0400

    Update Driver.java

--- 23_Coin/Driver.java
+++ 23_Coin/Driver.java
@@ -1,14 +1,15 @@
-/***
-    driver for class Coin
-    ~~~ SUGGESTED WORKFLOW: ~~~
-    1. Compile this file and run. Note anything notable.
-    2. Move the "TOP" line down, so that it is below the first statement.
-    (emacs: with cursor at beginning of TOP line, C-k C-k, DOWN, DOWN, C-y)
-    (your editor: ???)
-    3. Compile and run again.
-    4. Resolve errors one at a time until it works.
-    5. Repeat 2-4 until TOP meets BOTTOM.
-***/
+/**
+ *David Deng, Yat Long Chan
+ *APCS
+ *HW23 -- What Does Equality Look Like? / Comparing Variables from Different Objects
+ *2021-10-22
+ *Time Spent: 11 minutes
+ *
+ *DISCOS
+ *If a variable in another class is named exactly the same as a variable in your current class, use the Java keyword `this` to refer to the local variable.
+ *QCC
+ *No unresolved questions.
+ **/
 
 public class Driver {
 
@@ -18,21 +19,27 @@ public class Driver {
 
 
     //test default constructor
+
       Coin mine = new Coin();
+
       //test 1st overloaded constructor
       Coin yours = new Coin( "quarter" );
+
       //test 2nd overloaded constructor
       Coin wayne = new Coin( "dollar", "heads" );
+
       //test toString() methods of each Coin
       System.out.println("mine: " + mine);
       System.out.println("yours: " + yours);
       System.out.println("wayne: " + wayne);
+
       //test flip() method
       System.out.println("\nAfter flipping...");
       yours.flip();
       wayne.flip();
       System.out.println("yours: " + yours);
       System.out.println("wayne: " + wayne);
+
       //test equals() method
       if ( yours.equals(wayne) ) {
         System.out.println( "Matchee matchee!" );
@@ -40,8 +47,8 @@ public class Driver {
       else {
         System.out.println( "No match. Firestarter you can not be." );
       }
-      //test equals() method w/ null object
-      yours.equals(mine);
+      /*===================TOP==========================
+      ====================BOTTOM======================*/
 
   }//end main()
 
