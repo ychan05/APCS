e7c462d9c1564116647dd3fb34b3874dcad3ba32
Yat Long Chan <77934108+ychan2005@users.noreply.github.com>
2021-10-25 21:23:26 -0400

    Update Driver.java

--- 24_while/Driver.java
+++ 24_while/Driver.java
@@ -12,54 +12,78 @@
  **/
 
 public class Driver {
-  public static void loop(Coin c1, Coin c2, int x, int y, int birthyear){
-     int matchCtr = 0;
-     boolean start = true;
+  // flip until x heads have come up
+  public static void headCount(Coin c1,Coin c2, int max){
      int headsCt = c1.getHeadsCtr() + c2.getHeadsCtr();
      int flips = 0;
-     while (start)
+     while (headsCt < max)
      {
-	if ((matchCtr > 65536) && (matchCtr % birthyear == 0) && headsCt >= x && matchCtr >= y){
-	 start = false;
-	}
+	c1.flip();
+	c2.flip();
+        headsCt = c1.getHeadsCtr() + c2.getHeadsCtr();
+	flips += 1;
+     }
+    System.out.println("It took " + flips + " flips for " + max + " heads to come up.");
+  }
+  
+  //flip until y matches have occurred
+  public static void matchCount(Coin c1, Coin c2, int matches){
+     int matchCtr = 0;
+     int flips = 0;
+     while (matchCtr < matches)
+     {
         c1.flip();
         c2.flip();
 	if (c1.equals(c2)){
          matchCtr += 1;
 	}
-	headsCt = c1.getHeadsCtr() + c2.getHeadsCtr();     
 	flips += 1;
      }
-    System.out.println("It took " + flips + " flips for all conditions to be fullfilled.");
+    System.out.println("It took " + flips + " flips for " + matchCtr + " matches to occur.");
   }
+
+//flip until at least 65536 matches have come up, and the number of matches is divisible by the year of your birth.
+ public static void yearMatch(Coin c1, Coin c2, int birthyear){
+  int matchCtr = 0;
+  int flips = 0;
+  while (matchCtr < 65536){
+        c1.flip();
+        c2.flip();
+	if (c1.equals(c2)){
+         matchCtr += 1;
+	}
+	flips += 1;
+  }
+  while (matchCtr % birthyear != 0){
+        c1.flip();
+        c2.flip();
+	if (c1.equals(c2)){
+         matchCtr += 1;
+	}
+	flips += 1;
+ }
+    System.out.println("It took " + flips + " flips for the # of matches to be greater than 65536 and divisible by " + birthyear);
+ }
 
   public static void main( String[] args ) {
  /*===================TOP==========================
     //build Objects from blueprint specified by class Coin
-
-
     //test default constructor
-
       Coin mine = new Coin();
-
       //test 1st overloaded constructor
       Coin yours = new Coin( "quarter" );
-
       //test 2nd overloaded constructor
       Coin wayne = new Coin( "dollar", "heads" );
-
       //test toString() methods of each Coin
       System.out.println("mine: " + mine);
       System.out.println("yours: " + yours);
       System.out.println("wayne: " + wayne);
-
       //test flip() method
       System.out.println("\nAfter flipping...");
       yours.flip();
       wayne.flip();
       System.out.println("yours: " + yours);
       System.out.println("wayne: " + wayne);
-
       //test equals() method
       if ( yours.equals(wayne) ) {
         System.out.println( "Matchee matchee!" );
@@ -70,7 +94,9 @@ public class Driver {
       ====================BOTTOM======================*/
      Coin coin1 = new Coin("penny", "heads");
      Coin coin2 = new Coin("quarter", "heads");
-     loop(coin1, coin2, 70000, 69000, 2005);
+     headCount(coin1, 10);
+     matchCount(coin1, coin2, 10);
+     yearMatch(coin1, coin2, 2005);
   }//end main()
 
 }//end class
