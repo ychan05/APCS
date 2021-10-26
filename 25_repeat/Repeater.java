44b6f99800c749ddcf4266815f7884c5b246b81e
Yat Long Chan <77934108+ychan2005@users.noreply.github.com>
2021-10-26 16:20:31 -0400

    neater code

--- 25_repeat/Repeater.java
+++ 25_repeat/Repeater.java
@@ -4,57 +4,52 @@ APCS
 HW25 -- Do I repeat myself?/ make fence with while loop and recursive fxn.
 2021-10-26
 time spent: 0.3 hours
-
 DISCO
 The recursive method requires less lines than the method using a while loop.
 QCC
 none
 **/
-public class Repeater{
-
- public static String fenceW(int numPosts){
-  int count = 0;
-  String fence = "";
-  if (numPosts <= 0){
-   return fence;
-  } 
-  else {
-  while (count < numPosts){
-   count ++;
-   if (count == 1){
-    fence += "|";
-   }
-   else{
-    fence += "--|";
-   }
+public class Repeater {
+ //while loop method
+  public static String fenceW(int numPosts) {
+    int count = 0;
+    String fence = "";
+    if (numPosts <= 0) {
+      return fence;
+    } else {
+      while (count < numPosts) {
+        count++;
+        if (count == 1) {
+          fence += "|";
+        } else {
+          fence += "--|";
+        }
+      }
+    }
+    return fence;
   }
- }
-  return fence;
- }
-
- public static String fenceR(int numPosts){
-  String fence = "";
-  if (numPosts <= 0){
-   return fence;
+ //recursive method
+  public static String fenceR(int numPosts) {
+    String fence = "";
+    if (numPosts <= 0) {
+      return fence;
+    } else if (numPosts == 1) {
+      fence += "|";
+    } else {
+      fence += "|--";
+    }
+    return fence + fenceR(numPosts - 1);
   }
-  else if (numPosts == 1){
-   fence += "|";
-  }
-  else{
-   fence += "|--";
-  }
- return fence + fenceR(numPosts - 1);
- }
 
- public static void main(String[] args){
-  //While loop testing 
-  System.out.println(fenceW(1));
-  System.out.println(fenceW(2));
-  System.out.println(fenceW(3));
+  public static void main(String[] args) {
+    //While loop testing 
+    System.out.println(fenceW(1));
+    System.out.println(fenceW(2));
+    System.out.println(fenceW(3));
 
-  //recursive testing 
-  System.out.println(fenceR(1));
-  System.out.println(fenceR(2));
-  System.out.println(fenceR(3));
- }
+    //recursive testing 
+    System.out.println(fenceR(1));
+    System.out.println(fenceR(2));
+    System.out.println(fenceR(3));
+  }
 }
