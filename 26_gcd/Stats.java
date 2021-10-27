46f07ce901479bbc1b27045455977ba20e7efe16
Yat Long Chan <77934108+ychan2005@users.noreply.github.com>
2021-10-27 19:32:11 -0400

    fix algo stating

--- 26_gcd/Stats.java
+++ 26_gcd/Stats.java
@@ -9,15 +9,13 @@
    QCC
    - Which method would be the quickest or most efficient?
    ALGO
-    public static int gcdER(int a, int b) {
-        if (a == b) {
-            return a;
-        } else if (a > b) {
-            return gcdER((a - b), b);
-        } else {
-            return gcdER((b - a), a);
-        }
-    }
+   Note: We know that if the larger number is replaced with the difference of the larger - smaller, the GCD remains the same.
+   0) If a is equal to b, return either one of them. We chose to return a.
+   1) Otherwise, check if a > b.
+   2) If a > b, then we would find the difference of a - b and use that in step 3.
+   2.5) Otherwise, if a < b, we would find the difference of b - a and use that in step 3.
+   3) We then substitue as inputs a and b with the difference found in step 2 or 2.5 and the smaller number, respectively.
+   4) Use the new inputs to run through steps 0-3 until the two inputs are equal.
  */
 
 public class Stats {
