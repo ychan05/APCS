20db1c2cb28255f1946e1d3110b740388e07d279
Yat Long Chan <77934108+ychan2005@users.noreply.github.com>
2021-10-31 15:18:41 -0400

    add problems

--- cb/string-1/string1.java
+++ cb/string-1/string1.java
@@ -1,6 +1,6 @@
 public class string1 {
   //Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
-  
+
   public static String helloName(String name) {
     return ("Hello " + name + "!");
   }
@@ -8,7 +8,7 @@ public class string1 {
   /* Given two strings, a and b,
   return the result of putting them together in the order abba,
   e.g. "Hi" and "Bye" returns "HiByeByeHi". */
-  
+
   public static String makeAbba(String a, String b) {
     return (a + b + b + a);
   }
@@ -16,7 +16,7 @@ public class string1 {
   In this example, the "i" tag makes <i> and </i> which surround the word "Yay". 
   Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
   */
-  
+
   public String makeTags(String tag, String word) {
     return "<" + tag + ">" + word + "</" + tag + ">";
   }
@@ -34,10 +34,10 @@ public class string1 {
     String lastChars = str.substring(str.length() - 2);
     return lastChars + lastChars + lastChars;
   }
-  
+
   /*Given a string, return the string made of its first two chars, so the String "Hello" yields "He". 
   If the string is shorter than length 2, return whatever there is */
-  
+
   public static String firstTwo(String str) {
     if (str.length() < 2) {
       return str;
@@ -46,6 +46,26 @@ public class string1 {
     }
   }
 
+  //Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+  public String firstHalf(String str) {
+    return str.substring(0, str.length() / 2);
+  }
+
+  //Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+  public String withoutEnd(String str) {
+    return str.substring(1, str.length() - 1);
+  }
+
+  /* Given 2 strings, a and b, return a string of the form short+long+short, 
+  with the shorter string on the outside and the longer string on the inside. 
+  The strings will not be the same length, but they may be empty (length 0).*/
+  public String comboString(String a, String b) {
+    if (a.length() > b.length()) {
+      return b + a + b;
+    } else {
+      return a + b + a;
+    }
+  }
   public static void main(String[] args) {
     //helloName test
     System.out.println(helloName("Bob"));
