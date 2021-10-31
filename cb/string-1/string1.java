96ddb8f246901f73e9d73e4cf9ca77f4022df8be
Yat Long Chan <77934108+ychan2005@users.noreply.github.com>
2021-10-31 09:24:56 -0400

    add problems

--- cb/string-1/string1.java
+++ cb/string-1/string1.java
@@ -18,6 +18,30 @@ public class string1 {
     return "<" + tag + ">" + word + "</" + tag + ">";
   }
 
+  /* 
+Given an "out" string length 4, such as "<<>>", and a word, 
+return a new string where the word is in the middle of the out string, e.g. "<<word>>". */
+
+  public static String makeOutWord(String out, String word) {
+    return out.substring(0, 2) + word + out.substring(2);
+  }
+  /* Given a string, return a new string made of 3 copies of the last 2 chars of the original string. 
+  The string length will be at least 2. */
+
+  public static String extraEnd(String str) {
+    String lastChars = str.substring(str.length() - 2);
+    return lastChars + lastChars + lastChars;
+  }
+  /*Given a string, return the string made of its first two chars, so the String "Hello" yields "He". 
+  If the string is shorter than length 2, return whatever there is */
+  public static String firstTwo(String str) {
+    if (str.length() < 2) {
+      return str;
+    } else {
+      return str.substring(0, 2);
+    }
+  }
+
   public static void main(String[] args) {
     //helloName test
     System.out.println(helloName("Bob"));
@@ -27,5 +51,9 @@ public class string1 {
     System.out.println(makeAbba("Hi", "Bye"));
     System.out.println(makeAbba("Yo", "Alice"));
     System.out.println(makeAbba("What", "Up"));
+    //makeTags test
+    System.out.println(makeTags("i", "Yay"));
+    System.out.println(makeTags("i", "Hello"));
+    System.out.println(makeTags("cite", "Yay"));
   }
 }
