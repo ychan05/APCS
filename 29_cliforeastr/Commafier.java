29423af491853d1fed7dafff5e5279419d6d98e4
ychan2005 <ychan30@stuy.edu>
2021-11-01 19:49:47 -0400

    fixed recursive and made FOREACH LOOP

--- 29_cliforeastr/Commafier.java
+++ 29_cliforeastr/Commafier.java
@@ -17,21 +17,16 @@ public class Commafier{
     return ans;
   }
 
-  //doesnt work with zeros currently.
   public static String commafyR(int n){
     String strInt = Integer.toString(n);
     if (strInt.length() < 4){
       return strInt;
-    } else if (strInt.length() % 3 == 0){
-      String restNum = strInt.substring(3);
-      return strInt.substring(0, 3) + "," + commafyR(Integer.parseInt(restNum));
-    } else{
-      String restNum = strInt.substring((strInt.length() % 3));
-      return strInt.substring(0, (strInt.length() % 3)) + "," + commafyR(Integer.parseInt(restNum));
     }
+    int newN = Integer.parseInt(strInt.substring(0, strInt.length() - 3));
+    return commafyR(newN) + "," + strInt.substring(strInt.length() - 3);
   }
   public static void main (String[] args){
-    System.out.println(commafyF(1));
+    /* System.out.println(commafyF(1));
     System.out.println(commafyF(101));
     System.out.println(commafyF(1000));
     System.out.println(commafyF(12345));
@@ -43,6 +38,10 @@ public class Commafier{
     System.out.println(commafyR(1000));
     System.out.println(commafyR(12345));
     System.out.println(commafyR(4598765));
-    System.out.println(commafyR(100000000));
+    System.out.println(commafyR(100000000)); */
+    for(String s : args){
+      System.out.println("RECURSIVE TEST: " + commafyR(Integer.parseInt(s)) + "\n");
+      System.out.println("ITERATIVE TEST: " + commafyR(Integer.parseInt(s)) + "\n");
+    }
   }
 }
