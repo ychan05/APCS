bc9a295abdf0b5fabe8d8a927b77a8463d195ad9
ychan2005 <ychan30@stuy.edu>
2021-11-19 13:07:22 -0500

    RPG

---
+++ RPG/Monster.java
@@ -0,0 +1,53 @@
+public class Monster{
+  private int health; //just a default num
+  private int str;
+  private int precision;
+  private int ac;
+
+  public Monster(){
+    str = modifier();
+    precision = modifier();
+    health = 15 + modifier();
+    ac = 12 + modifier();
+  }
+
+  public int rollDie(int sideNum){
+    return (int) (Math.random() * sideNum) + 1;
+  }
+
+  public int modifier(){
+    return (int) (Math.random() * 3);
+  }
+
+  public int getAC(){
+    return ac;
+  }
+
+  public boolean isAlive(){
+    return health > 0;
+  }
+
+  public int getHP(){
+    return health;
+  }
+  public void takeDamage(int d){
+    health -= d;
+  }
+
+  public int attack(Protagonist p){
+    int damage = 0;
+    if (p.normalize()){
+      if (rollDie(20) > p.getAC()){
+        damage = rollDie(6) + str;
+      }
+    } else{
+      if (rollDie(20) > p.getAC()){
+        damage = (rollDie(6) + str) * 2;
+      }
+    }
+    p.takeDamage(damage);
+    return damage;
+  }
+
+
+}
---
+++ RPG/Protagonist.java
@@ -0,0 +1,70 @@
+public class Protagonist{
+  private int health; //just a default num
+  private int str;
+  private int precision;
+  private String heroName;
+  private int ac;
+  private boolean special;
+
+  public Protagonist(String name){
+    heroName = name;
+    str = modifier();
+    precision = modifier();
+    health = 30 + modifier();
+    ac = 13 + modifier();
+  }
+
+  public String getName(){
+    return heroName;
+  }
+
+  public int rollDie(int sideNum){
+    return (int) (Math.random() * sideNum) + 1;
+  }
+
+  public int modifier(){
+    return (int) (Math.random() * 5);
+  }
+
+  public boolean isAlive(){
+    return health > 0;
+  }
+
+  public int getHP(){
+    return health;
+  }
+  public void takeDamage(int d){
+    health -= d;
+  }
+
+  public int getAC(){
+    return ac;
+  }
+
+  public boolean specialize(){
+    special = true;
+    return special;
+  }
+
+  public boolean normalize(){
+    special = false;
+    return special;
+  }
+
+  public int attack(Monster m){
+    int damage = 0;
+    if (special){
+      if (rollDie(20) > m.getAC()){
+        damage = rollDie(8) + str;
+      }
+    } else{
+      if (rollDie(20) > m.getAC()){
+        damage = (rollDie(8) + str) * 2;
+      }
+    }
+    m.takeDamage(damage);
+    return damage;
+  }
+
+
+}
