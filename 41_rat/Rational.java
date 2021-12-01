5e2239c27553104876881ff086af5957d9c0e824
ychan2005 <ychan30@stuy.edu>
2021-12-01 18:38:50 -0500

    update

--- 41_rat/Rational.java
+++ 41_rat/Rational.java
@@ -11,8 +11,8 @@
 */
 
 public class Rational{
-  private int numerator;
-  private int denominator;
+  protected int numerator;
+  protected int denominator;
 
   public String toString(){
     return numerator + "/" + denominator;
@@ -24,42 +24,51 @@ public class Rational{
   }
 
   public Rational(int x, int y){
-    numerator = x;
-    denominator = y;
+    this();
+    if (y == 0){
+      System.out.println("ERROR: denominator cannot be 0");
+    } else {
+      numerator = x;
+      denominator = y;
+    }
   }
 
   public double floatVal(){
     return (double)numerator/denominator;
   }
 
-  public int getNum(){
-    return numerator;
-  }
-
-  public int getDen(){
-    return denominator;
-  }
-
   public void multiply(Rational n){
-    this.numerator *= n.getNum();
-    this.denominator *= n.getDen();
+    this.numerator *= n.numerator;
+    this.denominator *= n.denominator;
   }
 
   public void divide(Rational n){
-    this.numerator *= n.getDen();
-    this.denominator *= n.getNum();
+    if (n.numerator == 0){
+      System.out.println("ERROR: Dividing by 0");
+    } else {
+      this.numerator *= n.denominator;
+      this.denominator *= n.numerator;
+    }
   }
 
   public static void main(String[] args){
     Rational r = new Rational(4,5);
     Rational s = new Rational(1,3);
+    Rational t = new Rational();
+    System.out.println("=====TEST toString()=====");
     System.out.println(r.toString());
     System.out.println(s.toString());
+    System.out.println(t.toString());
+    System.out.println("=====TEST FLOATING POINT CONVERSION=====");
     System.out.println(r.floatVal());
     System.out.println(s.floatVal());
+    System.out.println("=====TEST multiply()=====");
     r.multiply(s);
     System.out.println(r.toString());
+    System.out.println("=====TEST divide()=====");
     r.divide(s);
+    System.out.println(r.toString());
+    r.divide(t);
     System.out.println(r.toString());
   }
 }
