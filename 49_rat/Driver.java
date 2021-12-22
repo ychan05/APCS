e234fd15a77bcfca90a8357ec00affe48d7bb90e
ychan2005 <ychan30@stuy.edu>
2021-12-22 17:13:54 -0500

    hw 49

---
+++ 49_rat/Driver.java
@@ -0,0 +1,18 @@
+public class Driver{
+    public static void main(String[] args){
+      Rational r = new Rational(4,5);
+      Rational s = new Rational(1,3);
+      Rational t = new Rational(10, 25);
+      Rational r1 = new Rational(4,5);
+      Rational r2 = r;
+      Integer i = 2;
+      System.out.println(r.compareTo(t)); // +
+      System.out.println(s.compareTo(r)); // - 
+      //System.out.println(r.compareTo(i)); throw error
+      System.out.println(r.equals(t)); // false
+      System.out.println(r.equals(i)); // false
+      System.out.println(r.equals(r1)); // true 
+      System.out.println(r.equals(null)); // false
+      System.out.println(r.equals(r2)); // true
+    }
+  }
