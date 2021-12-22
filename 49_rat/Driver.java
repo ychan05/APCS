4a627e64262a870292e611c929d74f6b7344f33c
ychan2005 <ychan30@stuy.edu>
2021-12-22 17:35:59 -0500

    account for - Rationals

--- 49_rat/Driver.java
+++ 49_rat/Driver.java
@@ -1,13 +1,13 @@
 public class Driver{
     public static void main(String[] args){
       Rational r = new Rational(4,5);
-      Rational s = new Rational(1,3);
-      Rational t = new Rational(10, 25);
+      Rational s = new Rational(-5,-3);
+      Rational t = new Rational(10, -25);
       Rational r1 = new Rational(4,5);
       Rational r2 = r;
       Integer i = 2;
       System.out.println(r.compareTo(t)); // +
-      System.out.println(s.compareTo(r)); // - 
+      System.out.println(r.compareTo(s)); // - 
       //System.out.println(r.compareTo(i)); throw error
       System.out.println(r.equals(t)); // false
       System.out.println(r.equals(i)); // false
