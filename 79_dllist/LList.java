eeddb432d10d3a2be67a3fa2e57514945690025d
Yat Long Chan <ychan30@stuy.edu>
2022-03-17 20:22:33 -0400

    commit

--- 79_dllist/LList.java
+++ 79_dllist/LList.java
@@ -1,29 +1,9 @@
 // Under Pressure: Julia Kozak, Anjini Katari, Yat Long Chan
 // APCS pd08
-// HW78 -- Double Up
-// 2022-03-17
+// HW79 -- A YouTube Host Are You
+// 2022-03-18
 // time spent: 0.5hrs
 // KTS used: 2
-
-/*
-  DISCO:    - An object with methods/values referenced cannot be null itself, but its attributes
-              can be set to null.
-            - No edge cases (except adding to the front) are needed for add(index).
-
-  QCC:      - Would it ever be useful to link the end back to _head?
-            - Can you use add(index) to add to the right end?
-
-  ALGO ADD: If the index being added to is 0, call add(newVal). Otherwise, create a newNode that
-            will be added. Set a temporary reference, tmp, to _head, and set it to the next node
-            until it is at index-1. Then, let tmp's next be newNode and newNode's previous be tmp,
-            and do the same for the other pair including newNode. Increment _size.
-
-  ALGO REM: If the index being removed is 0, then let the list start at _head's next, and set the
-            previous node to null. Otherwise, create a reference, tmp, originally set to _head,
-            and set it to the next node until it is at index-1. Then, set tmp's next node to the
-            one that is two after it, then set its new next node's previous to tmp. Decrement _size.
-*/
-
 
 
 /*****************************************************
@@ -33,7 +13,7 @@
  *****************************************************/
 
 public class LList implements List //your List interface must be in same dir
-{
+{ 
 
   //instance vars
   private DLLNode _head;
@@ -50,23 +30,18 @@ public class LList implements List //your List interface must be in same dir
 
 
   //--------------v  List interface methods  v--------------
-
+    
   public boolean add( String newVal )
   {
-    DLLNode tmp = new DLLNode( newVal, _head );
+    DLLNode tmp = new DLLNode( null, newVal, _head );
+    if (_head != null) _head.setPrev(tmp);
     _head = tmp;
-    _head.setPrev(null);
-
-    if (_size == 0) {
-      _head.setNext(_tail);
-    }
-
-
+    if (_tail == null) _tail = tmp;
     _size++;
     return true;
   }
 
-
+    
   public String get( int index )
   {
     if ( index < 0 || index >= size() )
@@ -109,7 +84,7 @@ public class LList implements List //your List interface must be in same dir
 
   //return number of nodes in list
   public int size() { return _size; }
-
+    
 
   //insert a node containing newVal at position index
   public void add( int index, String newVal ) {
@@ -117,11 +92,13 @@ public class LList implements List //your List interface must be in same dir
     if ( index < 0 || index >= size() )
 	    throw new IndexOutOfBoundsException();
 
-    DLLNode newNode = new DLLNode( newVal, null );
+    DLLNode newNode = new DLLNode( null, newVal, null );
 
     //if index==0, insert node before head node
-    if ( index == 0 )
+    if ( index == 0 ) 
 	    add( newVal );
+    else if (index == _size)
+      addLast(newVal);
     else {
 	    DLLNode tmp = _head; //create alias to head
 
@@ -131,10 +108,9 @@ public class LList implements List //your List interface must be in same dir
 
 	    //insert new node
 	    newNode.setNext( tmp.getNext() );
-      newNode.setPrev( tmp );
-      newNode.getNext().setPrev( newNode );
+      newNode.setPrev(tmp);
+      tmp.getNext().setPrev(newNode);
 	    tmp.setNext( newNode );
-
 
 	    //increment size attribute
 	    _size++;
@@ -158,10 +134,7 @@ public class LList implements List //your List interface must be in same dir
 
 	    //remove target node
 	    _head = _head.getNext();
-      if (_head != null) {
-        _head.setPrev(null);
-      }
-
+      if (_size > 1) _head.setPrev(null);
     }
     else {
 	    //walk to node before desired node
@@ -172,11 +145,8 @@ public class LList implements List //your List interface must be in same dir
 	    retVal = tmp.getNext().getCargo();
 
 	    //remove target node
+      if (index != _size - 1) tmp.getNext().getNext().setPrev(tmp);
 	    tmp.setNext( tmp.getNext().getNext() );
-      if (tmp.getNext() != null) {
-        tmp.getNext().setPrev(tmp);
-      }
-
     }
 
     //decrement size attribute
@@ -187,6 +157,15 @@ public class LList implements List //your List interface must be in same dir
 
   //--------------^  List interface methods  ^--------------
 
+  public boolean addLast( String newVal )
+  {
+    DLLNode tmp = new DLLNode( _head, newVal, null );
+    if (_tail != null) _tail.setNext(tmp);
+    _tail = tmp;
+    if (_head == null) _head = tmp;
+    _size++;
+    return true;
+  }
 
   // override inherited toString
   public String toString()
@@ -203,7 +182,7 @@ public class LList implements List //your List interface must be in same dir
 
 
   //main method for testing
-  /*public static void main( String[] args )
+  public static void main( String[] args )
   {
 
     LList james = new LList();
@@ -239,8 +218,8 @@ public class LList implements List //your List interface must be in same dir
     james.add(4,"phat");
     System.out.println( "...after add(4,phat): " );
     System.out.println( james );
-
-    System.out.println( "...after remove last: "
+    /*
+    System.out.println( "...after remove last: " 
                         + james.remove( james._size-1) );
     System.out.println( james );
 
@@ -251,7 +230,9 @@ public class LList implements List //your List interface must be in same dir
     System.out.println( james );
 
     System.out.println( "...after remove(0): " + james.remove(0) );
-    System.out.println( james );
-  }*/
+    System.out.println( james ); */
+  }
 
 }//end class LList
+
+
