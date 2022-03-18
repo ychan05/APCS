de909439eb447e8a63d43c7f49514099409cd031
ychan2005 <ychan30@stuy.edu>
2022-03-18 14:05:34 -0400

    commit

---
+++ cobo/011_elevens/Activity1 Starter Code/CardTester.java
@@ -0,0 +1,33 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		Card bob = new Card("seven", "diamond", 7);
+		Card ted = new Card("seven", "diamond", 7);
+		Card jef = new Card("eight", "spade", 8);
+
+		System.out.println("Testing accessors...");
+
+		System.out.println(bob.suit());
+		System.out.println(bob.rank());
+		System.out.println(bob.pointValue());
+		System.out.println(ted.suit());
+		System.out.println(ted.rank());
+		System.out.println(ted.pointValue());
+		System.out.println(jef.suit());
+		System.out.println(jef.rank());
+		System.out.println(jef.pointValue());
+
+		System.out.println("Testing matches...");
+		System.out.println(bob.matches(ted));
+		System.out.println(bob.matches(jef));
+
+
+	}
+}
---
+++ cobo/011_elevens/Activity2  Starter Code/DeckTester.java
@@ -0,0 +1,20 @@
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		String[] ranks = {"A", "B", "C"};
+		String[] suits = {"Giraffes", "Lions"};
+		int[] values = {2,1,6};
+		Deck bob = new Deck(ranks, suits, values);
+		System.out.println(bob.size());
+		System.out.println(bob.deal());
+		System.out.println(bob.size());
+
+	}
+}
