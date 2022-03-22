7cc45392de7dc99cc393188d003ec8efdfbb1775
ychan2005 <ychan30@stuy.edu>
2022-03-22 14:07:05 -0400

    commit

---
+++ 80/ListTester.java
@@ -0,0 +1,19 @@
+/***
+ * class ListTester
+ * ...for putting your LList through its paces
+ *
+ * Assumes local List.java (interface),
+ *  overriding List interface from standard Java library
+ **/
+
+public class ListTester
+{
+  public static void main( String[] args ) 
+  {
+    LList bob = new LList();
+    LList<String> hi = new LList<String>();
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+  }//end main()
+
+}//end class
