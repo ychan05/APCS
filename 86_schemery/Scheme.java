8fb93c1595d1b480b9f914866068395bc6332dcd
Yat Long Chan <ychan30@stuy.edu>
2022-03-31 16:41:00 -0400

    finish methods

--- 86_schemery/Scheme.java
+++ 86_schemery/Scheme.java
@@ -3,9 +3,11 @@
  * Simulates a rudimentary Scheme interpreter
  *
  * ALGORITHM for EVALUATING A SCHEME EXPRESSION:
- *   1. Steal underpants.
- *   2. ...
- *   5. Profit!
+ *   1. Starting from end of expression. If char is not open paren or operation, add to stack.
+ *   2. If char is open paren, operate on operands in stack using matching operation until top of stack is a close paren, 
+ *      which can be removed. Then add result of operation to stack.
+ *   3. Repeat 1 and 2 until entire expression is looked through.
+ *   5. Simplified expression should be at the top of the stack.
  *
  * STACK OF CHOICE: ____ by ____
  * b/c ...
@@ -24,7 +26,25 @@ public class Scheme
    **/
   public static String evaluate( String expr )
   {
+    Stack<String> numbers = new ALStack<String>();
+    String a[] = expr.split("\\s+");
+    int op = 0;
 
+    for (int i = a.length - 1; i >= 0; i --) {
+
+      if (a[i].equals("(")) {
+        String res = (unload(op, numbers));
+        numbers.pop();
+        numbers.push(res);
+      }
+
+      else if (a[i].equals("+")) {op = 1;}
+      else if (a[i].equals("-")) {op = 2;}
+      else if (a[i].equals("*")) {op = 3;}
+      else {numbers.push(a[i]);}
+      
+    }
+    return numbers.peekTop();
   }//end evaluate()
 
 
@@ -36,20 +56,28 @@ public class Scheme
    **/
   public static String unload( int op, Stack<String> numbers )
   {
+    Integer res = Integer.parseInt(numbers.pop());
 
+    while (!( (numbers.peekTop()).equals(")") ) ) {
+
+      if (op == 1) {res += Integer.parseInt(numbers.pop());}
+      else if (op == 2) {res -= Integer.parseInt(numbers.pop());}
+      else if (op == 3) {res *= Integer.parseInt(numbers.pop());}
+    }
+    return Integer.toString(res);
   }//end unload()
 
 
   /*
   //optional check-to-see-if-its-a-number helper fxn:
   public static boolean isNumber( String s ) {
-  try {
-  Integer.parseInt(s);
-  return true;
-  }
-  catch( NumberFormatException e ) {
-  return false;
-  }
+    try {
+      Integer.parseInt(s);
+      return true;
+    }
+    catch( NumberFormatException e ) {
+      return false;
+    }
   }
   */
 
@@ -58,26 +86,26 @@ public class Scheme
   public static void main( String[] args )
   {
 
+    String zoo1 = "( + 4 3 )";
+    System.out.println(zoo1);
+    System.out.println("zoo1 eval'd: " + evaluate(zoo1) );
+    //...7
+    
+    String zoo2 = "( + 4 ( * 2 5 ) 3 )";
+    System.out.println(zoo2);
+    System.out.println("zoo2 eval'd: " + evaluate(zoo2) );
+    //...17
+    
+    String zoo3 = "( + 4 ( * 2 5 ) 6 3 ( - 56 50 ) )";
+    System.out.println(zoo3);
+    System.out.println("zoo3 eval'd: " + evaluate(zoo3) );
+    //...29
+    
+    String zoo4 = "( - 1 2 3 )";
+    System.out.println(zoo4);
+    System.out.println("zoo4 eval'd: " + evaluate(zoo4) );
+    //...-4
     /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
-      String zoo1 = "( + 4 3 )";
-      System.out.println(zoo1);
-      System.out.println("zoo1 eval'd: " + evaluate(zoo1) );
-      //...7
-
-      String zoo2 = "( + 4 ( * 2 5 ) 3 )";
-      System.out.println(zoo2);
-      System.out.println("zoo2 eval'd: " + evaluate(zoo2) );
-      //...17
-
-      String zoo3 = "( + 4 ( * 2 5 ) 6 3 ( - 56 50 ) )";
-      System.out.println(zoo3);
-      System.out.println("zoo3 eval'd: " + evaluate(zoo3) );
-      //...29
-
-      String zoo4 = "( - 1 2 3 )";
-      System.out.println(zoo4);
-      System.out.println("zoo4 eval'd: " + evaluate(zoo4) );
-      //...-4
       ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
   }//main()
 
