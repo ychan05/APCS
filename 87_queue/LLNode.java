36b2d0dda187b2b48bfbc33672a7f68e78e1ed39
Yat Long Chan <ychan30@stuy.edu>
2022-04-01 15:51:35 -0400

    methods

--- 87_queue/LLNode.java
+++ 87_queue/LLNode.java
@@ -47,47 +47,4 @@ public class LLNode<T>
   }
   //--------------^  MUTATORS  ^--------------
 
-
-
-
-  //main method for testing
-  public static void main( String[] args )
-  {
-
-    //Below is an exercise in creating a linked list...
-
-    //Create a node
-    LLNode first = new LLNode( "cat", null );
-
-    //Create a new node after the first
-    first.setNext( new LLNode( "dog", null ) );
-
-    //Create a third node after the second
-    first.getNext().setNext( new LLNode( "cow", null ) );
-
-    /* A naive list traversal, has side effects.... ??
-       while( first != null ) {
-       System.out.print( first );
-       first = first.getNext();
-       }
-       */
-    //System.out.println(first.getNext());
-
-
-    //Q: when head ptr moves to next node in list, what happens to the node it just left?
-    // That node becomes null (first gets set to null and you can no longer reference it)
-
-    //...so better: ?
-    // create a new reference
-    LLNode current = first;
-    while (current != null) {
-      System.out.println( current );
-      current = current.getNext();
-    }
-    System.out.println(first);
-    System.out.println(first.getNext());
-    System.out.println(first.getNext().getNext());
-
-  }//end main
-
 }//end class LLNode
--- 87_queue/NodeQueue.java
+++ 87_queue/NodeQueue.java
@@ -1,33 +1,64 @@
 public class NodeQueue<T> implements Queue<T> {
-    LLNode<T> _node;
+    LLNode<T> _head;
     int _size = 0;
 
     public NodeQueue() {
-        _node = new LLNode<T>(null, null);
+        _head = new LLNode<T>(null, null);
     }
  
     public void enqueue(T x) {
-        if (_size == 0) {
-            _node.setCargo(x);
+        if (isEmpty()) {
+            _head.setCargo(x);
             _size ++;
             return;
         }
 
         if (_size == 1) {
-            _node.setNext( new LLNode<T>(x, null) );
+            _head.setNext( new LLNode<T>(x, null) );
             _size ++;
             return;
         }
 
-        LLNode<T> head = _node;
-        while (head.getNext() != null) {
-            head = head.getNext();
+        LLNode<T> ptr = _head;
+        while (ptr.getNext() != null) {
+            ptr = ptr.getNext();
         }
-        head.setNext( new LLNode<T>(x, null) );
+        ptr.setNext( new LLNode<T>(x, null) );
         _size ++;
     }
 
     public T dequeue() {
+        if (isEmpty()) return null;
+        T ret = _head.getCargo();
+        _head = _head.getNext();
+        _size --;
+        return ret;
+    }
+
+    public boolean isEmpty() {
+        return _size == 0;
+    }
+
+    public T peekFront() {
+        if (isEmpty()) return null;
+        return _head.getCargo();
+    }
+
+    public static void main(String[] args) {
+        Queue<Object> q = new NodeQueue<Object>();
+        q.enqueue(1);
+        q.enqueue("hi");
+        q.enqueue(true);
+        q.enqueue(2.0);
         
+        while(!q.isEmpty()) {
+            System.out.println(q.peekFront());
+            q.dequeue();
+        }
+
+        q.dequeue();
+        System.out.println(q.peekFront());
+
+
     }
 }
